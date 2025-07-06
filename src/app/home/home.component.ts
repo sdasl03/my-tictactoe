05b4a2a1261c8b2bd4bbed3cdea6f3eaa2352f4a
Samuel Lira <sdasl03@github.com>
2025-07-06 12:00:54 +0100

    test added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GameService } from '../game.service';
 import { RouterLink } from '@angular/router';
 
